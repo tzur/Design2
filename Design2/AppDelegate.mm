7ca8c3d09bc0243ee0dc4736d188376b3fbdaa72
zur tene <zur@lightricks.com>
2016-12-06 17:45:08 +0200

    Finished documenting.

---
+++ Design2/AppDelegate.mm
@@ -0,0 +1,14 @@
+// Copyright (c) 2016 Lightricks. All rights reserved.
+// Created by Zur Tene.
+
+#import "AppDelegate.h"
+
+#import "imports.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@implementation AppDelegate
+
+@end
+
+NS_ASSUME_NONNULL_END
--- Design2/Processor.h
+++ Design2/Processor.h
@@ -1,10 +1,20 @@
 // Copyright (c) 2016 Lightricks. All rights reserved.
 // Created by Zur Tene.
 
+#import <Foundation/Foundation.h>
+
 NS_ASSUME_NONNULL_BEGIN
 
+@protocol Operation;
+
+/// Object that responsible for processing input texture into output texture with respect to its
+/// current values. Implementations of this protocol should define actual way of setting the input
+/// and output texture and the adjusting the processor values.
 @protocol Processor <NSObject>
 
+/// Run the processor.
+- (void)process;
+  
 @end
 
 NS_ASSUME_NONNULL_END
